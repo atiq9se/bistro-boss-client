75da6718d5911be62fbb3cd37800a5b50292361b
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-01-02 23:55:29 +0600

    cover page design

--- src/pages/Home/Menu/Menu/Menu.jsx
+++ src/pages/Home/Menu/Menu/Menu.jsx
@@ -1,4 +1,6 @@
 import { Helmet } from 'react-helmet-async';
+import Cover from '../../../../Shared/Cover/Cover';
+import menuImg from '../../../../assets/menu/banner3.jpg'
 
 const Menu = () => {
     return (
@@ -6,6 +8,7 @@ const Menu = () => {
             <Helmet>
                 <title>Hello World</title>
             </Helmet>
+            <Cover img={menuImg} title="our menu"></Cover>
         </div>
     );
 };
