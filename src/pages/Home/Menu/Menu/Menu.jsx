f3887a96b0a982f1ff599c0cdd7949a43897c409
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-01-02 07:40:19 +0600

    menu part and react helmet adding

---
+++ src/pages/Home/Menu/Menu/Menu.jsx
@@ -0,0 +1,13 @@
+import { Helmet } from 'react-helmet-async';
+
+const Menu = () => {
+    return (
+        <div>
+            <Helmet>
+                <title>Hello World</title>
+            </Helmet>
+        </div>
+    );
+};
+
+export default Menu;
