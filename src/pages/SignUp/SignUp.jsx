c7f991bbfa35f3ad602b4ce0f6e93c0a462fcbe6
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-01-07 23:30:28 +0600

    create user with email and password

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,9 +1,19 @@
+import { useContext } from "react";
 import { useForm } from "react-hook-form";
+import { AuthContext } from '../../provider/AuthProvider';
 
 
 const SignUp = () => {
     const {register, handleSubmit, formState: { errors }} = useForm()
-    const onSubmit = (data) => console.log(data)
+    const {createUser} = useContext(AuthContext)
+
+    const onSubmit = (data) => {
+        createUser(data.email, data.password)
+        .then(result=>{
+            const loggedUser = result.user;
+            console.log(loggedUser)
+        })
+    }
 
     return (
         <div className="hero bg-base-200 min-h-screen">
@@ -29,13 +39,21 @@ const SignUp = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email" className="input input-bordered" />
+                            <input type="email" {...register("email", { required: true})} placeholder="email" className="input input-bordered" />
+                            {errors.email?.type==="required" && <p className="text-red-500">Name is required</p>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password" className="input input-bordered" />
+                            <input type="password" {...register("password", { 
+                                required: true, 
+                                minLength:6, 
+                                pattern:/(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])/ 
+                            })} placeholder="password" className="input input-bordered" />
+                            {errors.password?.type==="required" && <p className="text-red-500">Name is required</p>}
+                            {errors.password?.type==="minLength" && <p className="text-red-500">Minlenth is required</p>}
+                            {errors.password?.type==="pattern" && <p className="text-red-500">One uppercase one lowercase one spacial character</p>}
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                             </label>
