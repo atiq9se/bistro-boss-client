77e0535cddff21a6ecede3e2bcb8018f579378b9
Md.Atiqur Rahman <atiq9se@gmail.com>
2025-01-11 22:25:07 +0600

    create cart list and delete cart item

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -1,15 +1,21 @@
-import { FaAd, FaCalendar, FaHome, FaList, FaShoppingCart } from "react-icons/fa";
+import { FaAd, FaCalendar, FaHome, FaList, FaSearch, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import useCart from "../hooks/useCart";
 
 const Dashboard = () => {
+    const [cart] = useCart();
     return (
         <div className="flex">
             <div className="w-64 min-h-full bg-orange-400">
                 <ul className="menu">
                     <li> <NavLink to="/dashboard/home"><FaHome></FaHome> User Home</NavLink></li>
                     <li> <NavLink to="/dashboard/reservation"><FaCalendar></FaCalendar> Reservation</NavLink></li>
-                    <li> <NavLink to="/dashboard/review"><FaShoppingCart></FaShoppingCart> My Cart</NavLink></li>
+                    <li> <NavLink to="/dashboard/review"><FaShoppingCart></FaShoppingCart> My Cart{cart.length}</NavLink></li>
                     <li> <NavLink to="/dashboard/review"><FaAd></FaAd> Add a review</NavLink></li>
+                    <li> <NavLink to="/dashboard/booking"><FaList></FaList> My Bookings</NavLink></li>
+                    <div className="divider"></div>
+                    <li> <NavLink to="/"><FaHome></FaHome> Home</NavLink></li>
+                    <li> <NavLink to="/menu"><FaSearch></FaSearch>Menu</NavLink></li>
                     <li> <NavLink to="/dashboard/booking"><FaList></FaList> My Bookings</NavLink></li>
                 </ul>
             </div>
